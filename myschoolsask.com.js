0442506daa6b7f6078025322643f8c057ede7b83
Mark Strendin <mark.strendin@lskysd.ca>
2019-03-21 11:28:00 -0600

    Overhaul of how the settings work, now that I have a better understanding of how browser storage works

--- myschoolsask.com.js
+++ myschoolsask.com.js
@@ -2,9 +2,9 @@ function onError(error) {
   console.log(`Error: ${error}`);
 }
 
-function doPreventTimeout(options) {
+function onSettingsLoaded(settings) {
 	// Check if we're allowed to prevent timeouts
-	if (options.lCanPreventTimeout === true) {
+	if (settings.lCanPreventTimeout == true) {
 
 		// Check if the hours are within the boundaries		
 		// Hard code these boundaries for the time being - we currently don't want these to be user settable.
@@ -16,8 +16,13 @@ function doPreventTimeout(options) {
 			$("body").before("<script language=\"javascript\">setInterval(function() { var d = new Date(); getParentX2Window().lastUserEvent = d.getTime(); }, 5000);</script>");
 		} 
 	}
+
+	// Check if we should show the "It Works" banner
+	if (settings.lShowItWorksBanner == true) {
+		$("body").before("<div style=\"font-size: 8pt; margin: 0; padding: 0; width: 100%; background-color: yellow; color: black;text-align: center;font-family: sans-serif;\">MySchoolSask Enhancement Suite is able to modify the contents of this page.</div>");		 
+	}
 }
 
 // Load the options and then handle the response we get back from browser storage
-var getting = browser.storage.sync.get("lCanPreventTimeout");  
-getting.then(doPreventTimeout, onError);
+var savedSettings = browser.storage.sync.get();  
+savedSettings.then(onSettingsLoaded, onError);
--- options.js
+++ options.js
@@ -4,32 +4,36 @@ function onError(error) {
 
 function doSave(e) {
   e.preventDefault();
+  console.log("saving prevent timeout:" + document.querySelector("#chkPreventTimeout").checked);
+  console.log("saving it works banner: " + document.querySelector("#chkShowItWorks").checked);
+
   browser.storage.sync.set({
-    lCanPreventTimeout: document.querySelector("#chkPreventTimeout").checked
-  });    
+    lCanPreventTimeout: document.querySelector("#chkPreventTimeout").checked,
+    lShowItWorksBanner: document.querySelector("#chkShowItWorks").checked
+  });
 }
 
 function updateOption(result) {
   console.log(result);
 }
 
-function restoreOptions() {
-  function setCurrentChoice(result) {
-    var newval = result.lCanPreventTimeout || false;
-    console.log("lCanPreventTimeout: " + newval);
-    if (newval == true) {
-      document.querySelector("#chkPreventTimeout").checked = true;
-      console.log("yes"); 
-    } else {
-      console.log("no");
-      document.querySelector("#chkPreventTimeout").checked = false;
-    }
-  }
+function updateFields_Callback(settings) {
+  var blnPreventTimeout = settings["lCanPreventTimeout"] === true || false;
+  var blnItWorks = settings["lShowItWorksBanner"] === true || false;
 
-  var getting = browser.storage.sync.get("lCanPreventTimeout");
-  getting.then(setCurrentChoice, onError);
+  console.log("Updating prevent timeout to: " + blnPreventTimeout);
+  console.log("Updating show it works to: " + blnItWorks);
+
+  document.querySelector("#chkPreventTimeout").checked = blnPreventTimeout;
+  document.querySelector("#chkShowItWorks").checked = blnItWorks;
+
+}
+
+function updateFields() {
+  var savedSettings = browser.storage.sync.get();
+  savedSettings.then(updateFields_Callback, onError);
 }
 
 // Listen for save button being pressed
-document.addEventListener("DOMContentLoaded", restoreOptions);
+document.addEventListener("DOMContentLoaded", updateFields);
 document.querySelector("form").addEventListener("submit", doSave);
